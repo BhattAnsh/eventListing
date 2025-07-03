028233e857529e7259036ef890c66aefcaf2396d
BhattAnsh <ansbhatt140@gmail.com>
2025-07-03 20:09:55 +0530

    prepared the folder structure and basic functionalies moving forward to the ui and filtering

---
+++ src/pages/events/[id].tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { GetStaticPaths, GetStaticProps } from "next";
+import events from "@/data/data.json";
+import { Event } from "@/types/event";
+
+type Prop{
+    event:Event,
+}
+export default function EventDetails({event}:Props){
+
+    (
+        <div></div>
+    )
+}
---
+++ src/types/event.ts
@@ -0,0 +1,14 @@
+export type Event ={
+    id:string,
+    title:string,
+    description:string,
+    date:string,
+    time:string,
+    location:string,
+    category:string,
+    price:string,
+    image:string,
+    organizer:string,
+    capacity:number,
+    tags:Array<string>
+}
